5dc30136e9a7472b61917346f24af466e872ad44
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-08-10 10:43:38 +0500

    fix: update pgxpool package and update database config

--- config/database.go
+++ config/database.go
@@ -10,17 +10,15 @@ import (
 var DB *pgxpool.Pool
 
 func ConnDB() (*pgxpool.Pool, error) {
-
 	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		return nil, err
 	}
+
 	if err := dbpool.Ping(context.Background()); err != nil {
 		return nil, err
 	}
 
 	DB = dbpool
-
 	return dbpool, nil
-
 }
