68e78ac60c10e1fea511a1b72e14ef9dc1f9a175
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-08-10 10:38:56 +0500

    go mod initialize and add database configuration

---
+++ config/database.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var DB *pgxpool.Pool
+
+func ConnDB() (*pgxpool.Pool, error) {
+
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, err
+	}
+	if err := dbpool.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	DB = dbpool
+
+	return dbpool, nil
+
+}
