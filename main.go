cf769a80d960ddee77b1ee5ab6b0cda8a97ab537
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-08-10 10:54:08 +0500

    feat: add env load and initialize database functions into main.go file

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"task/config"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	// Loading .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Database instance
+	db, err := config.ConnDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 }
